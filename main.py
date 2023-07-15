4fa0cb4e065e29fa7e2bf36e4d88d0dbf5745f8c
Chris G <isotechnic@gmail.com>
2023-07-15 21:31:07 +0100

    error handling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import requests
+import requests, os
 from datetime import date
-import os
 
 # Set API key from environment variable
 api_key = os.environ.get('ABSTRACT_API_KEY')
@@ -12,14 +11,11 @@ month = today.strftime("%-m")
 day_of_month = today.strftime("%-d")
 
 # Make the API request
-response = requests.get(f'https://holidays.abstractapi.com/v1/?api_key={api_key}&country=GB&year={year}&month={month}&day={day_of_month}')
-
-# Parse the response code
-return_code = response.status_code
-
-if return_code != 200:
-  print("Response code is not 200")
-  exit
+try:
+  response = requests.get(f'https://holidays.abstractapi.com/v1/?api_key={api_key}&country=GB&year={year}&month={month}&day={day_of_month}')
+  response.raise_for_status()
+except requests.exceptions.RequestException as e:
+  raise SystemExit(e)
 
 # Parse the response
 api_response = response.json()
